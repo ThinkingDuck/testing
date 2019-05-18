d164c514b4527e6478e785884fb4f20e6853e441
Karl Friesen <karl@entrupy.com>
2019-05-18 19:45:43 -0400

    Adding a basic urwid interface

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,11 @@
-print("Hello, world")
-#allo
+import urwid
+
+def show_or_exit(key):
+    if key in ('q', 'Q'):
+        raise urwid.ExitMainLoop()
+    txt.set_text(repr(key))
+
+txt = urwid.Text(u"Enter any key and it will show up here.")
+fill = urwid.Filler(txt, 'top')
+loop = urwid.MainLoop(fill, unhandled_input=show_or_exit)
+loop.run()
